a09599bbc48b1f023f6096f4bf73d39ab251622b
dievgen <duboriz.evg@gmail.com>
2015-12-09 19:00:36 +0200

    version 0.0.3. some changes in code

--- weather.py
+++ weather.py
@@ -1,42 +1,38 @@
 import serial
 import datetime
 import time
-import os
-import sys
 
-
-ser = serial.Serial('COM3', 600)
+ser = serial.Serial('COM7', 9600)
 ser.flushInput()
 ser.close()
 ser.open()
 ts = time.time()
 filename = datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d%H%M%S_log')
 myfile = open(filename+'.csv','w')
-#myerrorlog = open('err'+filename+'.log','w')
-
 while 1:
 	while(ser.inWaiting()==0):
 		pass
-	pressure = tempbmp080=tempDHT=tempDS=humidDHT=0
+	pressure = tempbmp080=tempDHT=tempDS=humidDHT=COsensor=""
 	myfile = open(filename+'.csv','a')
 	ts = time.time()
 	datestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y/%m/%d')
 	timestamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
 
 	buffer = ser.readline()
-	comdata = buffer[2:28]
+	comdata = buffer[2:35]
 	strdata = str(comdata)
 	comdataA = strdata.split(',')
 	try:
-		pressure = str(comdataA[0])
-		tempbmp080 = str(comdataA[1])
-		tempDS = str(comdataA[2])
-		tempDHT = str(comdataA[3])
-		humidDHT = str(comdataA[4])
-		#COsensor = str(comdatA[5])
+		pressure = comdataA[0]
+		tempbmp080 = comdataA[1]
+		tempDS = comdataA[2]
+		tempDHT = comdataA[3]
+		humidDHT = comdataA[4]
+		COsensor = comdataA[5]
 	except:
 		pass
-
-	myfile.write(datestamp+';'+timestamp+';'+pressure+';'+tempDHT+';'+humidDHT+'\n')
-	print(datestamp+'/'+timestamp+','+pressure+','+tempDHT+','+tempDS+','+tempbmp080+','+humidDHT)
+	output = '%s/%s,%s,%s,%s,%s,%s,%s' %(datestamp,timestamp,pressure,tempbmp080,tempDS,tempDHT,humidDHT,COsensor)
+	myfile = open(filename+'.csv','a')
+	myfile.write(str(output))
+	print(str(output))
 	myfile.close()
