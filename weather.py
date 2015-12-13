b39146a8227751680ce0c9e2a94990038e0785df
dievgen <duboriz.evg@gmail.com>
2015-12-13 22:32:06 +0200

    version 0.0.5. had made a class for work with serial port

--- weather.py
+++ weather.py
@@ -4,43 +4,49 @@ import time
 import os
 
 
-port_number = input('input serial port number: ')
+number = input('input serial port number: ')
 #ser = serial.Serial('COM'+ser_number, 9600)
 #ser.flushInput()
 #ser.close()
 #ser.open()
 ts = time.time()
 
-class serial_port:
-	def __init__(self):
-		try:
-			self.port = serial_port.read_port(port_number)
-			self.ser = serial.serial_port(
-				port = serial_port.read_port(port_number),
-				baudrate = 9600,
-				parity = serial.PARITY_NONE,
-				stopbits = serial.STOPBITS_ONE,
-				bytesize = serial.EIGHTBITS,
-				timeout = 1
-			)
-		except SerialException:
-			print('error connection')
-			os._exit()
-	def read_port(self):
+class Serial_port:
+	def __init__(self, port, baudrate = 9600, timeout = 5):
+		self.port = serial.Serial(
+			port = 'COM'+number,
+			baudrate = baudrate,
+			timeout = timeout,
+			writeTimeout=timeout)
 
+	def open(self):
+		if(self.port.isOpen() == False):
+			self.port.open()
+		else:
+			self.port.close()
+			self.port.open()
+
+	def close(self):
+		self.port.close()
+
+	def recvline(self):
+		return self.port.readline()
+
+Serial_port(number)
+Serial_port(number).open()
 
 #filename = datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d%H%M%S_log')
 #myfile = open(filename+'.csv','w')
 while 1:
-	while(ser.inWaiting()==0):
-		pass
+	#while(Serial_port(number).inWaiting()==0):
+	#	pass
 	pressure = tempbmp080=tempDHT=tempDS=humidDHT=COsensor=""
 #	myfile = open(filename+'.csv','a')
 	ts = time.time()
 	datestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y/%m/%d')
 	timestamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
 
-	buffer = ser.readline()
+	buffer = Serial_port(number).recvline()
 	comdata = buffer[2:35]
 	strdata = str(comdata)
 	comdataA = strdata.split(',')
@@ -58,3 +64,4 @@ while 1:
 #	myfile.write(str(output))
 	print(str(output))
 #	myfile.close()
+#	Serial_port(number).close()
