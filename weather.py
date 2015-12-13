8c6f1bfdcb5fa6937ac8540413ec9fa60fdd098b
dievgen <duboriz.evg@gmail.com>
2015-12-13 16:26:32 +0200

    version 0.0.4. add serial port selector

--- weather.py
+++ weather.py
@@ -9,13 +9,13 @@ ser.flushInput()
 ser.close()
 ser.open()
 ts = time.time()
-filename = datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d%H%M%S_log')
-myfile = open(filename+'.csv','w')
+#filename = datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d%H%M%S_log')
+#myfile = open(filename+'.csv','w')
 while 1:
 	while(ser.inWaiting()==0):
 		pass
 	pressure = tempbmp080=tempDHT=tempDS=humidDHT=COsensor=""
-	myfile = open(filename+'.csv','a')
+#	myfile = open(filename+'.csv','a')
 	ts = time.time()
 	datestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y/%m/%d')
 	timestamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
@@ -34,7 +34,7 @@ while 1:
 	except:
 		pass
 	output = '%s/%s,%s,%s,%s,%s,%s,%s' %(datestamp,timestamp,pressure,tempbmp080,tempDS,tempDHT,humidDHT,COsensor)
-	myfile = open(filename+'.csv','a')
-	myfile.write(str(output))
+#	myfile = open(filename+'.csv','a')
+#	myfile.write(str(output))
 	print(str(output))
-	myfile.close()
+#	myfile.close()
